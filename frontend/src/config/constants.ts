aa24164cb6c5524c37d22a959528546e42cd6738
Saveur Bikorimana <bikorimanaxavier@gmail.com>
2025-05-22 00:39:03 +0200

    updated backend url

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api'; 
+export const API_URL =  "https://citizen-complaints.onrender.com"; //'http://localhost:8080/api'; 
